037d41998fa56954e71fe0052086a5db55a0caaf
Akshay Vs <akvsx@proton.me>
2026-08-03 23:51:16 +0530

    feat: add ValidateAccountStatusUseCase to storefront-auth module

--- apps/core/src/modules/storefront-users/index.ts
+++ apps/core/src/modules/storefront-users/index.ts
@@ -1,1 +1,4 @@
+export * from './application/use-cases/get-storefront-users.use-case';
+export * from './domain/errors/storefront-user-not-found.error';
+export * from './domain/ports/storefront-user-repository.port';
 export * from './storefront-users.module';
