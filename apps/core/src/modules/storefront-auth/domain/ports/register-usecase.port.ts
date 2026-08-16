af0e07a6f14acceaa296c5fe8121efb11646b8fa
Akshay Vs <akvsx@proton.me>
2026-08-16 16:42:59 +0530

    feat: add auto-login user upon registration

--- apps/core/src/modules/storefront-auth/domain/ports/register-usecase.port.ts
+++ apps/core/src/modules/storefront-auth/domain/ports/register-usecase.port.ts
@@ -1,13 +1,18 @@
 import { IUseCase } from '@common/interfaces/use-case.interface';
 import type { StorefrontUserRegister } from '@ferrite/schema/storefront-auth/register.zodschema';
-import type { StorefrontUserResponse } from '@ferrite/schema/storefront-auth/storefront-user.zodschema';
 import { IncompleteConfigurationError } from '@store/domain/errors/incomplete-configuration.error';
 import { EmailAlreadyRegisteredError } from '../errors/email-already-registered.error';
 
 export const STOREFRONT_REGISTER_UC = Symbol('STOREFRONT_REGISTER_UC');
 
+import type { LoginResult } from './login-usecase.port';
+
 export type IStorefrontRegisterUser = IUseCase<
-	StorefrontUserRegister & { storeId: string },
-	StorefrontUserResponse,
+	StorefrontUserRegister & {
+		storeId: string;
+		ipAddress: string;
+		userAgent: string;
+	},
+	LoginResult,
 	EmailAlreadyRegisteredError | IncompleteConfigurationError | Error
 >;
